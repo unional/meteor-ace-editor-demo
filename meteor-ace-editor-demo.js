9050ea1db9df9390c66d0aa097ec9fe1f4e9f9d5
Homa Wong <hwong@paloaltonetworks.com>
2014-04-24 01:44:35 -0700

    Change ace-editor path to use local copy (branched for uniteNote)

--- meteor-ace-editor-demo.js
+++ meteor-ace-editor-demo.js
@@ -1,11 +1,16 @@
 if (Meteor.isClient) {
-  Template.editor.rendered = function () {
-    var editor = ace.edit("editor");
-  };
+    Template.editor.rendered = function() {
+        var editor = ace.edit("editor");
+        editor.session.setMode("ace/mode/un");
+        editor.setTheme("ace/theme/un_default");
+        editor.setOptions({
+            enableLiveAutocompletion: [myCompleter]
+        });
+    };
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
